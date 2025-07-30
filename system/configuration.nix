43ed2d822020d0e211c8dc4283d766ff5c446396
nainteeth <knusperpommes@gmail.com>
2025-07-30 08:55:17 +0200

    Add kio-admin for dolphin --sudo

--- system/configuration.nix
+++ system/configuration.nix
@@ -93,7 +93,7 @@
         Current = "catppuccin-mocha";
         CursorTheme = "Adwaita";
         CursorSize = 24;
-        background = "/etc/nixos/wallpapers/";
+        background = "/etc/nixos/wallpapers/blue-wave.png";
       };
       Wayland = {
         SessionDir = "${pkgs.hyprland}/share/wayland-sessions";
@@ -126,7 +126,7 @@
     kitty
     wofi # change to home manager
     kdePackages.dolphin
-    kio-admin
+    kdePackages.kio-admin
     unzip
     wl-clipboard
     xdg-utils
