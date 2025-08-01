5063057930acd64724bce33d9e8623b0e9a9c4b5
nainteeth <knusperpommes@gmail.com>
2025-08-01 11:49:17 +0200

    enable hyprview plugin

--- home/hyprland/plugins.nix
+++ home/hyprland/plugins.nix
@@ -4,7 +4,7 @@
   # Popular Hyprland plugins - uncomment the ones you want to use
   
   # Hyprspace - Overview plugin similar to GNOME's activities
-  # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprspace
+  inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprspace
   
   # Hyprbars - Custom title bars for windows
   # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
@@ -17,9 +17,6 @@
   
   # Borders-plus-plus - Enhanced border customization
   # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.borders-plus-plus
-
-  # Example of enabling hyprbars:
-  # inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
 
   # You can also add custom plugins here by building them manually:
   # (pkgs.callPackage ./path/to/custom-plugin.nix { 
