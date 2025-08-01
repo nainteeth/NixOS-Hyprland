c1787b905fe4ff2589f8b91d6033ba43f8bc2b2d
nainteeth <knusperpommes@gmail.com>
2025-08-01 11:40:27 +0200

    Fix xdg-portal for hyprland

--- system/desktop.nix
+++ system/desktop.nix
@@ -30,16 +30,6 @@
   
   services.displayManager.sessionPackages = [ pkgs.hyprland ];
 
-  # XDG Desktop Portal for Wayland
-  xdg.portal = { 
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
-
   # Security
   security.polkit.enable = true;
 
