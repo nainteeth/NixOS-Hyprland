372ed44f860f0fdaab045d6eb3ec392a9fcdd11b
nainteeth <knusperpommes@gmail.com>
2026-01-07 18:51:23 +0100

    Reduce bloat

--- home/zen-browser.nix
+++ home/zen-browser.nix
@@ -32,37 +32,6 @@
         "toolkit.telemetry.unified" = false;
         "toolkit.telemetry.server" = "";
 
-        # Zen UI specifics
-        "zen.view.use-multiple-toolbars" = true;
-        "zen.view.sidebar.show-essentials" = false;
-        "zen.view.show-pinned-tabs" = false;
-
-        # New Tab Shortcuts
-        "browser.newtabpage.activity-stream.default.sites" = "https://github.com,https://nixos.org,https://reddit.com";
-
-        # Toolbar layout
-        "browser.uiCustomization.state" = builtins.toJSON {
-          placements = {
-            "nav-bar" = [
-              "zoom-controls"
-              "history-panelmenu"
-              "back-button"
-              "forward-button"
-              "stop-reload-button"
-              "urlbar-container"
-              "downloads-button"
-              "unified-extensions-button"
-            ];
-            "toolbar-menubar" = [ "menubar-items" ];
-            "PersonalToolbar" = [ "personal-bookmarks" ];
-          };
-          seen = [ "zoom-controls" "history-panelmenu" "developer-button" ];
-          dirtyAreaCache = [ "nav-bar" "PersonalToolbar" "TabsToolbar" ];
-          currentVersion = 20;
-          newElementCount = 4;
-        };
-      };
-      
       search = {
         force = true;
         default = "ddg";
@@ -76,24 +45,5 @@
       };
     };
   };
-
-  # This must be outside the programs.zen-browser block
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = let
-      desktopFile = "zen.desktop"; 
-    in {
-      "text/html" = desktopFile;
-      "x-scheme-handler/http" = desktopFile;
-      "x-scheme-handler/https" = desktopFile;
-      "x-scheme-handler/about" = desktopFile;
-      "x-scheme-handler/unknown" = desktopFile;
-      "application/xhtml+xml" = desktopFile;
-      "application/x-extension-htm" = desktopFile;
-      "application/x-extension-html" = desktopFile;
-      "application/x-extension-shtml" = desktopFile;
-      "application/x-extension-xhtml" = desktopFile;
-      "application/x-extension-xht" = desktopFile;
-    };
-  };
+};
 }
