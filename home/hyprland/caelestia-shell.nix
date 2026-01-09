2b0c009dbbc46d4962c15ffff4bad61ab8078f89
nainteeth <knusperpommes@gmail.com>
2026-01-09 16:25:16 +0100

    fix screenshot keybind

--- home/hyprland/caelestia-shell.nix
+++ home/hyprland/caelestia-shell.nix
@@ -6,8 +6,8 @@
     "${mainMod} SHIFT, E, global, caelestia:session"
     "${mainMod} SHIFT, S, global, caelestia:showall"
     "${mainMod} Ctrl, L, global, caelestia:lock"
-    ", Print, global, caelestia:screenshot"
-    "SHIFT, Print, global, caelestia:screenshot -f"
+    ", Print, exec, caelestia screenshot"
+    "SHIFT, Print, global, caelestia:screenshotFreeze"
     "${mainMod}, D, global, caelestia:dashboard"
     "${mainMod}, S, global, caelestia:sidebar"
   ];
