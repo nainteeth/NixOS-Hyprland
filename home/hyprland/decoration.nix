25350f45b7e29cdde89e24f393b767602094063b
nainteeth <knusperpommes@gmail.com>
2026-01-09 20:08:46 +0100

    customize kitty transparency

--- home/hyprland/decoration.nix
+++ home/hyprland/decoration.nix
@@ -15,6 +15,6 @@
     enabled = true;
     size = 3;
     passes = 1;
-    vibrancy = 0.1696;
   };
+
 }
--- home/kitty.nix
+++ home/kitty.nix
@@ -2,6 +2,10 @@
 {
   programs.kitty = {
     enable = true;
+    settings = {
+      background_opacity = "0.65";
+      dynamic_background_opacity = true;
+    };
       extraConfig = ''
       confirm_os_window_close 0 count-background
     '';
